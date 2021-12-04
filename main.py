d817b5ef93e504a321fcf60a795b88d3de7e855e
yahya benzha <yahya_benzha@um5.ac.ma>
2021-12-04 01:03:51 +0100

    data inputs and cleaning done

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from tkinter import *
+import tkinter as tk
 import os
 import zipfile
 from time import sleep
@@ -17,8 +18,8 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 import re
 # -----------------------------------------------Set up Proxies --------------------------------------
-sites = ['instagram.com', 'facebook.com']
-niches = ['dogs', 'cats']
+sites = []
+niches = []
 # configure proxy
 
 
@@ -186,6 +187,14 @@ def Main_SCRAPPER(driver, list_of_proxies):
 
 
 def main():
+    if site.get() != '':
+        sites.append(site.get())
+    if niche.get() != '':
+        niches.append(niche.get())
+
+    if len(sites) == 0 or len(niches) == 0:
+        print("Eror")
+        return
 
     List_of_Proxies = [
         {
@@ -222,23 +231,93 @@ def main():
     Main_SCRAPPER(driver, List_of_Proxies)
 
 
-def pr():
-
-    print(site.get(), niche.get())
-
-
-View = Tk()
+PROXY_LIST = ['iu']
+sites = []
+niches = []
 
 
-View.geometry("1000x600")
+def printe():
+    print(PROXY_LIST)
 
-btn = Button(View, text="Scrap", command=pr)
-btn.pack()
 
-site = StringVar()
-niche = StringVar()
-sites_entry = Entry(textvariable=site, width=30)
-niches_entry = Entry(textvariable=niche, width=30)
-sites_entry.place(x=60, y=60)
-niches_entry.place(x=60, y=80)
-View.mainloop()
+def get_data():
+    sites = sites_entry.get("1.0", "end").splitlines()
+
+    niches = niches_entry.get("1.0", "end").splitlines()
+
+    Proxy_config = {
+        'PROXY_HOST': '',
+        'PROXY_PORT': '',
+        'PROXY_USER': '',
+        'PROXY_PASS': ''
+
+    }
+
+    proxies = proxies_entry.get("1.0", "end")
+    for proxy in proxies.splitlines():
+        configs = proxy.split(':')
+        Proxy_config['PROXY_HOST'] = configs[0]
+        Proxy_config['PROXY_PORT'] = configs[1]
+        Proxy_config['PROXY_USER'] = configs[2]
+        Proxy_config['PROXY_PASS'] = configs[3]
+        PROXY_LIST.append(Proxy_config)
+
+    print(PROXY_LIST)
+
+
+# Container  of all franmes == root window
+Global_View = Tk()
+Global_View.geometry("720x1000")
+Global_View.config(bg="#054861")
+
+Container = Frame(Global_View)
+
+# frames inside
+
+Left_frame = Frame(Global_View)
+Right_frame = Frame(Global_View)
+
+Right_frame.config(bg="blue", width=900)
+Right_frame.pack(fill=tk.BOTH, side=RIGHT)
+# set frame to the left
+
+Left_frame.config(bg="green", width=900)
+Left_frame.pack(fill=tk.BOTH, side=LEFT)
+# left frame components
+site_title = Label(Left_frame, text="WEBSITES AREA")
+niche_title = Label(Left_frame, text="NICHES AREA")
+
+site_title.config(bg="green", font=("Montserrat", 22),
+                  height="1", fg="white")
+site_title.place(x=230, y=20)
+
+niche_title.config(bg="green", font=("Montserrat", 22),
+                   height="1", fg="white")
+
+niche_title.place(x=230, y=240)
+
+
+sites_entry = Text(Left_frame, height=10)
+niches_entry = Text(Left_frame, height=10)
+
+
+sites_entry.place(x=25, y=60)
+niches_entry.place(x=25, y=290)
+
+proxies_title = Label(Left_frame, text="PROXIES AREA")
+proxies_title.config(bg="green", font=("Montserrat", 22),
+                     height="1", fg="white")
+proxies_title.place(x=230, y=490)
+
+proxies_entry = Text(Left_frame, height=10)
+
+proxies_entry.place(x=25, y=530)
+
+
+btn = Button(Left_frame, text="Connect", command=get_data)
+
+btn.place(x=250, y=800)
+btn1 = Button(Left_frame, text="Connect", command=printe)
+
+btn1.place(x=250, y=900)
+Global_View.mainloop()
